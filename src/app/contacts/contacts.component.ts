e202adc5a16d373c2e837dc35ae77b5b75eb560f
Damien Arondel <damien.arondel@icloud.com>
2018-08-11 17:59:46 +0200

    #4 Implement the sort feature over contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -12,11 +12,38 @@ export class ContactsComponent implements OnInit {
 
   contacts: Contact[];
 
+  columnIds = ['name', 'phone_number', 'address'];
+
+  sortedColumn: string;
+  ascendingSort: boolean;
+
   constructor(private contactService: ContactService) {
   }
 
   ngOnInit() {
     this.contactService.getContacts().subscribe(contacts => this.contacts = contacts);
+  }
+
+  /**
+   * Sort the contact array according to the event source element id.
+   *
+   * @param event the click event
+   */
+  onSort(event: MouseEvent) {
+    const columnId = event.srcElement.id;
+
+    this.ascendingSort = this.sortedColumn === columnId ? !this.ascendingSort : true;
+    this.sortedColumn = columnId;
+
+    this.contacts.sort((contact1, contact2) => {
+      let result = contact1[columnId].localeCompare(contact2[columnId]);
+
+      if (!this.ascendingSort) {
+        result = -result;
+      }
+
+      return result;
+    });
   }
 
 }
